309db1fe68496abb6b1e0d524643406d605abed3
Mustafizur Rohman <mustafizur.rohman@weischer.net>
2022-08-19 16:27:51 +0200

    Singleton impl

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -1,1 +1,13 @@
 ﻿namespace Singleton;
+
+public class Logger
+{
+    private static readonly Lazy<Logger> _LazyLogger = new(() => new Logger());
+    
+    public static Logger Instance => _LazyLogger.Value;
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"Mesage to log: {message}");
+    }
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.Title = "Singleton";
+﻿using Singleton;
+
+Console.Title = "Singleton";
+
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if (instance1 == instance2 && instance2 == Logger.Instance)
+    Console.WriteLine("Same instance");
+
+instance1.Log("Logging from instance1");
+instance2.Log("Logging from instance2");
+
+Console.ReadKey();
